f0cd65806ca153073af639bd58b8c5257c3ac305
Emese Elkind <emese.elkind@gmail.com>
2025-02-24 21:48:41 -0500

    dl

---
+++ DeepLearning.py
@@ -0,0 +1,21 @@
+"""
+TreatmentFlow
+Deep Learning: Patient Priority Prediction
+
+Automate assignment of patient priority (1-5) using Triage information 
+Training based on 500k+ observation dataset of triage information associated with patient priority
+
+By Adam Neto and Emese Elkind
+Started: February 2025
+
+CISC 352: Artificial Intelligence
+​"""
+
+
+"""
+Step 1: Import Libraries. 
+Step 2: Load and Preprocess the Data. 
+Step 3: Build the Model. 
+Step 4: Train the Model. 
+Step 5: Evaluate the Model. 
+"""
