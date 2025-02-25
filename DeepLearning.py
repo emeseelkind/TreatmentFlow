83ff01ca294d325ccce83ea22d342b87059049bc
Emese <151964707+emeseelkind@users.noreply.github.com>
2025-02-25 13:51:34 -0500

    Update DeepLearning.py

--- DeepLearning.py
+++ DeepLearning.py
@@ -19,3 +19,41 @@ Step 3: Build the Model.
 Step 4: Train the Model. 
 Step 5: Evaluate the Model. 
 """
+import pandas as pd
+
+# Replace 'path/to/file.csv' with the actual file path
+file_path = 'C:/Users/emese/Desktop/TreatmentFlow/hospital_triage_patient_data.csv'
+df = pd.read_csv(file_path)
+"""
+# Display the first 5 rows
+print(df.head())
+
+# Display the column names
+print(df.columns)
+
+# Get information about the DataFrame (e.g., number of rows, columns, and data types)
+print(df.info())
+
+# Get basic statistics about numerical columns
+print(df.describe())
+"""
+
+
+# Extract the target variable (patient priority) and the features (triage information)
+"""
+Extract a specific column:
+age_data = df['age']
+print(age_data.head())
+
+Extract multiple columns:
+demographics = df[['age', 'gender', 'ethnicity']]
+print(demographics.head())
+
+Extract a specific row by index:
+first_row = df.iloc[0]
+print(first_row)
+
+Filter rows based on a condition:
+older_patients = df[df['age'] > 50]
+print(older_patients.head())
+"""
