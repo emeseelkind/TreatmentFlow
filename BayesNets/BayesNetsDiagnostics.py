1213a5616d337178f6640cc32c216fd9834f23f8
Adam Neto <adammichaelneto@gmail.com>
2025-03-09 17:39:46 -0400

    added comments to lay out our initial plan with BN

--- BayesNets/BayesNetsDiagnostics.py
+++ BayesNets/BayesNetsDiagnostics.py
@@ -19,8 +19,42 @@ with open("BayesNets/symbipredict_2022.csv") as symptom_info:
     for row in reader:
         diagnostics.append(row)
 
-symptoms = diagnostics[0][:-1]  # list of symptoms
-diagnostics = diagnostics[1:]   # list of diagnostic observations (with prognosis)
+diagnoses = [row[-1] for row in diagnostics[1:]]        # list of possible diagnoses
+diagnosis_options = []  # list of each possible diagnosis (without duplicates)
 
-print(symptoms)
+i = 0
+while i < len(diagnoses):
+    if diagnoses[i] not in diagnosis_options:
+        diagnosis_options.append(diagnoses[i])
+    i += 1
 
+symptoms = diagnostics[0][:-1] # list of all symptom variables
+symptom_bools = [row[:-1] for row in diagnostics[1:]]   # 2d array of booleans referring to symptoms
+
+# goal with CPTs: get the conditional probabilities of each diagnosis given symptoms
+
+# print(len(diagnoses))
+print(diagnosis_options)
+print(len(diagnosis_options))
+# print(len(symptoms))
+# print(len(symptom_bools[0]))
+
+# for each possible diagnosis, we want:
+    # P(Di | S1, S2, ..., Sn)
+
+
+# goal for Bayes Net: make every independent pair of symptoms siblings, rather than parents/children
+    # X and Y are independent if
+        # for all x, y: P(x,y) = P(x) * P(y) = P(x) * P(y|x)
+        # for all x, y: P(x|y) = P(x)
+    # conditional independence of X and Y, given Z
+        # for all x, y, z: P(x, y|z) = P(x|z) * P(y|z)
+        # for all x, y, z: P(x|z, y) = P(x|z)     
+
+    # each node in the BN refers to a probability table
+        # if node A has parents X and Y, A refers to the probability table P(A | X,Y)
+            # P(xi | x1, ..., x_(i-1)) = P(xi | parents(Xi)
+
+    # bayes net should allow us to output the posterior probability of a diagnosis D given some observed evidence S
+        # P(D|S) = (P(S|D) * P(D)) / P(S)
+        # display the ~10 most probable diagnoses given a set of evidence (boolean symptom values)
