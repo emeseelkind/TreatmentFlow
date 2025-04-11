2c31362b2d186351bb6d7917e559d260b736422e
Adam Neto <adammichaelneto@gmail.com>
2025-04-11 17:21:30 -0400

    made significant progress on menu-based UI

--- TreatmentFlow_Lite.py
+++ TreatmentFlow_Lite.py
@@ -10,7 +10,7 @@ Text-based UI for interacting with all 3 components of TreatmentFlow:
 - Bayesian Netowrk                - patient disease document generator
 """
 
-from CSP.HospitalClasses import Patient, HospitalRecords
+from CSP.HospitalClasses import Patient, HospitalRecords, print_time
 from CSP.GreedyBedAssignment import Scheduler
 from DeepLearning import DeepLearning as dl
 import os
@@ -21,20 +21,28 @@ import numbers
 
 class PatientDatabase:
 
-    def __init__(self, NUM_PATIENTS, NUM_BEDS):
-        self.NUM_PATIENTS = NUM_PATIENTS
-        self.NUM_BEDS = NUM_BEDS
-
-        # store patient and hospital data
-        self.hospital = HospitalRecords(NUM_BEDS)
+    def __init__(self, num_patients, num_beds):
+        self.num_patients = num_patients
+        
+        # create databases
+        self.update_hospital(num_beds)
         self.patient_db = []
         
-        self.user_id = 0
+        # note that user values are not yet set
+        self.user_id = -1
         
         # load pandas patient directory (from CTAS spreadsheets)
         current_dir = os.path.dirname(__file__)
         patient_samples_dir = os.path.join(current_dir, "CTAS_files")
         self.df = dl.load_data_printless(patient_samples_dir)
+
+    def update_hospital(self, num_beds):
+        # set number of beds
+        self.num_beds = num_beds
+
+        # create hosptial objects
+        self.hospital = HospitalRecords(num_beds)
+        self.scheduler = Scheduler(self.hospital)
 
     def add_user_info(self, arrival):
 
@@ -42,7 +50,7 @@ class PatientDatabase:
         random_index = np.random.choice(len(self.df))
         random_row = self.df.iloc[random_index].drop("esi")
 
-        self.user_id = len(self.patient_db)
+        self.user_id = 0
 
         patient = {}
         patient["id"] = self.user_id
@@ -50,12 +58,21 @@ class PatientDatabase:
         patient["ctas"] = int(0) # ** MUST USE DL TO PREDICT SYMPTOMS
         patient["symptoms"] = random_row
 
-        self.patient_db.append(patient) 
+        self.patient_db.insert(0, patient)
+        self.user_id = 0
 
     def fill_db(self):
 
+        # reset database
+        if self.user_id < 0:
+            self.patient_db = []
+        else:
+            temp = self.patient_db[0]
+            self.patient_db = []
+            self.patient_db.append(temp)
+
         # sample patients from database
-        sample_indices = np.random.choice(range(len(self.df)), size=self.NUM_PATIENTS-1, replace=False)        
+        sample_indices = np.random.choice(range(len(self.df)), size=self.num_patients-1, replace=False)        
         sample_df = self.df.iloc[sample_indices]
         ctas_values = sample_df["esi"]
         sample_patients = sample_df.drop('esi', axis=1)
@@ -80,16 +97,18 @@ class PatientDatabase:
             
             current_patient = Patient()
             current_patient.fill_patient_stats(patient["id"], patient["arrival"], patient["ctas"])
+
+            patient["object"] = current_patient
             self.hospital.patient_list.append(current_patient)
 
-        Scheduler(self.hospital).run_hospital(printing)
+        self.scheduler.run_hospital(printing)
 
 
 class Menu:
 
     def __init__(self):
-        self.NUM_PATIENTS = 0
-        self.NUM_BEDS = 0
+        self.num_patients = 0
+        self.num_beds = 0
         self.patient_db = None
 
     def select_int(self, message, lb, ub):
@@ -106,43 +125,160 @@ class Menu:
             except ValueError:
                 output = input("Input must be an integer: ")
 
+    def observe_patient(self, patient_id):
+        if self.patient_data.user_id < 0:
+            patient_dict = self.patient_data.patient_db[patient_id - 1]
+        else:
+            patient_dict = self.patient_data.patient_db[patient_id]
+
+        while True:
+            # core menu
+            print(f"\nPatient {patient_id}: ")
+            print(" 1. Service times")
+            print(" 2. Symptom list")
+            print(" 3. Patient document")
+            print(" 4. Exit")
+
+            response = self.select_int("Choice", 1, 4)
+            match response:
+                case 1:
+                    print(f"\nPatient {patient_id} service time info:")
+                    
+                    # print out patient arrival, service time, discharge time
+                    print(f"Arrival: {print_time(patient_dict['arrival'])}")
+                    print(f"Service time: {print_time(patient_dict['object'].service_time)}")
+
+                    if patient_dict["object"].service_start < 0:
+                        print(f"Patient {patient_id} was never served.")
+                    else:
+                        print(f"Waiting time: {print_time(patient_dict['object'].get_waiting_time())}")
+                        print(f"First served: {print_time(patient_dict['object'].service_start)}")
+
+                case 2:
+                    print(f"\nPatient {patient_id} symptom info:")
+
+                    # print out patient symptoms
+                    for symptom_name, symptom_value in patient_dict["symptoms"].items():
+                        if isinstance(symptom_value, numbers.Number):
+                            if symptom_value > 0:
+                                print(f"{symptom_name}: {symptom_value}")
+
+                case 3:
+                    print(f"PLACEHOLDER: Bayes output for patient {patient_id}")
+
+                case 4:
+                    return
+
+    def access_patients(self):
+        
+        self.patient_data.assign_beds()
+
+        while True:
+
+            # core menu
+            print("\nPatient database: ")
+            print(" 1. Select user")
+            print(" 2. Select other patient")
+            print(" 3. Exit")
+
+            response = self.select_int("Choice", 1, 3)
+            match response:
+                case 1:
+                    if self.patient_data.user_id < 0:
+                        print("User data must be uploaded before access.")
+                    else:
+                        self.observe_patient(0)
+
+                case 2:
+                    # prevent list overflow errors
+                    min_required = 0 if self.patient_data.user_id < 0 else 1
+                    
+                    if len(self.patient_data.patient_db) > min_required:
+                        self.observe_patient(self.select_int("Select patient ID", 1, self.num_patients - 1))
+                    else:
+                        print("Additional patient data must be uploaded before access.")
+
+                case 3:
+                    return
+
+    # def update_stats(self):
+        
     def run_menu(self):
 
-        print("Welcome to TreatmentFlow Lite!")
+        print("\nWelcome to TreatmentFlow Lite!")
 
-        # general statistics inquiry
-        self.NUM_PATIENTS = self.select_int("How many patients should be in the hospital?", 0, 7000)
-        self.NUM_BEDS = self.select_int("How many beds should be in the hospital?", 0, 1000)
+        # initial setup
+        self.num_patients = self.select_int("How many patients should be in the hospital?", 1, 7000)
+        self.num_beds = self.select_int("How many beds should be in the hospital?", 1, 1000)
 
         # building database
-        self.patient_data = PatientDatabase(self.NUM_PATIENTS, self.NUM_BEDS)
+        self.patient_data = PatientDatabase(self.num_patients, self.num_beds)
 
         # enter patient info (user is always patient ID 0)
         user_arrival = self.select_int("User arrival time (by the minute)", 0, 1439)
         self.patient_data.add_user_info(user_arrival)
-        
-        # ** final product should allow users to perform triage survey
-
-        # randomly sampling existing patient profile
-        print("Randomly filling patient profiles...")
         self.patient_data.fill_db()
 
-        print("Printing patient db:") # TEMPORARY - FOR TESTING/SHOWCASE
-        for row in self.patient_data.patient_db:
-            print()
-            print(row["id"])
-            print(row["ctas"])
-            print(row["arrival"])
+        while True:
 
-            for symptom_name, symptom_value in row["symptoms"].items():
-                # print(f"Symptoms: {symptom_name} - {symptom_value}")
-                if isinstance(symptom_value, numbers.Number):
-                    if symptom_value > 0:
-                        print(f"Symptoms: {symptom_name} - {symptom_value}")
+            # core menu
+            print("\nPlease select an option: ")
+            print(" 1. Update user info")
+            print(" 2. Update hospital size")
+            print(" 3. Access patient database")
+            print(" 4. Update patient list")
+            print(" 5. Print bed assignments")
+            print(" 6. Quit")
 
-        # assign patients to beds
-        self.patient_data.assign_beds(True)
+            response = self.select_int("Choice", 1, 6)
+            match response:
+                case 1:
+                    # enter patient info (user is always patient ID 0)
+                    user_arrival = self.select_int("User arrival time (by the minute)", 0, 1439)
+                    self.patient_data.add_user_info(user_arrival)
 
+                    # ** final product should allow users to perform triage survey
+                case 2:
+                    num_beds = self.select_int("Number of beds", 1, 1000)
+                    self.patient_data.update_hospital(num_beds)
+
+                case 3:
+                    self.access_patients()
+
+                case 4:
+                    self.num_patients = self.select_int("Number of patients", 1, 7000)
+                    self.patient_data.num_patients = self.num_patients
+
+                    # randomly sampling existing patient profile
+                    self.patient_data.fill_db()
+
+                case 5:
+                    # assign patients to beds
+                    self.patient_data.assign_beds(True)
+
+                case 6:
+                    return
+
+
+            
+            
+
+            
+
+            # print("Printing patient db:") # TEMPORARY - FOR TESTING/SHOWCASE
+            # for row in self.patient_data.patient_db:
+            #     print()
+            #     print(f"Row: {row}")
+            #     print(row["id"])
+            #     print(row["ctas"])
+            #     print(row["arrival"])
+
+            #     for symptom_name, symptom_value in row["symptoms"].items():
+            #         # print(f"Symptoms: {symptom_name} - {symptom_value}")
+            #         if isinstance(symptom_value, numbers.Number):
+            #             if symptom_value > 0:
+            #                 print(f"Symptoms: {symptom_name} - {symptom_value}")
+        
 
 my_menu = Menu()
 my_menu.run_menu()
